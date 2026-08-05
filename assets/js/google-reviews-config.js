a5ced8e3149466beec3470f1748808c2d2b602cf
Aditya Pendyala <aditya.pendyala@rawasi-capital.com>
2026-08-05 17:42:24 +0400

    Add Google API key for Google Reviews integration

--- assets/js/google-reviews-config.js
+++ assets/js/google-reviews-config.js
@@ -1,6 +1,6 @@
 window.CORAL_GOOGLE_REVIEWS = window.CORAL_GOOGLE_REVIEWS || {
   endpoint: "",
-  apiKey: "",
+  apiKey: "AIzaSyCqxVhiAjZTmx6Y-fNGrEg-A8YIHEx-OPE",
   placeId: "",
   placeQuery: "Coral Spa B.K-2 Tower 3/13A Vishnu Puri Kanpur",
   mapsUrl: "https://www.google.com/maps/search/?api=1&query=Coral%20Spa%20B.K-2%20Tower%203%2F13A%20Vishnu%20Puri%20Kanpur"
